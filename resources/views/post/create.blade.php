7b38a32c37d25528af2a372ad8761f43a543a6ae
patricionicolas <patricionicolas@live.cl>
2020-08-05 14:00:16 -0400

    Ingreso de post
    Primera versión del formulario que ingresa datos.

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -7,11 +7,12 @@
                 <div class="card-header">Ingrese los datos necesarios</div>
 
                 <div class="card-body">
-                <form action="">
+                <form action="{{ url('/storepost') }}" method="post">
+                    @csrf
                     <div class="row">
                         <div class="col-md-3">Titulo</div>
                         <div class="col-md-9">
-                            <input class="form-control" type="text" placeholder="Graduacion" id="titulo">
+                            <input class="form-control" type="text" placeholder="Graduacion" name="titulo" id="titulo" required>
                         </div>
 
                         <div class="col-md-12"> <br> </div>
@@ -19,7 +20,7 @@
                         <div class="col-md-3">Descripción</div>
                         <div class="col-md-9">
                             <div class="form-group">
-                                <textarea class="form-control" id="descripcion" rows="4"></textarea>
+                                <textarea class="form-control" name="descripcion" id="descripcion" rows="4" required></textarea>
                             </div>
                         </div>
 
@@ -27,21 +28,21 @@
 
                         <div class="col-md-3">Url</div>
                         <div class="col-md-9">
-                            <input class="form-control" type="url" placeholder="www.uct.cl" id="titulo">
+                            <input class="form-control" type="url" placeholder="www.uct.cl" name="url" id="url">
                         </div>
 
                         <div class="col-md-12"> <br> </div>
 
                         <div class="col-md-3">Fecha</div>
                         <div class="col-md-9">
-                            INPUT
+                            <input class="col-md-12" type="date" name="fecha" required>
                         </div>
 
                         <div class="col-md-12"> <br> </div>
 
                         <div class="col-md-3">¿Publico?</div>
                         <div class="col-md-9">
-                            <input type="radio" name="publico" value="1"> Si <br>
+                            <input type="radio" name="publico" value="1" required> Si <br>
                             <input type="radio" name="publico" value="0"> No <br>
                         </div>
 
