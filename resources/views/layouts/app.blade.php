8e6a3f9f2b5314a2eec9d9259c60664904b230f5
patricionicolas <patricionicolas@live.cl>
2020-08-06 13:59:34 -0400

    Formulario Ingreso de datos
    Se cambia la apariencia del formulario de datos.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,10 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link type="text/css" href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/fontawesome.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/solid.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/light.css') }}">
 </head>
 <body>
     <div id="app">
--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -4,21 +4,23 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">Ingrese los datos necesarios</div>
+                <div class="card-header">
+                    <h5 class="card-title"> <strong>  Ingresa tu recuerdo </strong></h5>
+                </div>
 
                 <div class="card-body">
                 <form action="{{ url('/storepost') }}" method="post">
                     @csrf
                     <div class="row">
-                        <div class="col-md-3">Titulo</div>
-                        <div class="col-md-9">
+                        <div class="col-md-2 font-weight-bold">Titulo</div>
+                        <div class="col-md-10">
                             <input class="form-control" type="text" placeholder="Graduacion" name="titulo" id="titulo" required>
                         </div>
 
                         <div class="col-md-12"> <br> </div>
 
-                        <div class="col-md-3">Descripción</div>
-                        <div class="col-md-9">
+                        <div class="col-md-2 font-weight-bold">Descripción</div>
+                        <div class="col-md-10">
                             <div class="form-group">
                                 <textarea class="form-control" name="descripcion" id="descripcion" rows="4" required></textarea>
                             </div>
@@ -26,30 +28,30 @@
 
                         <div class="col-md-12"> <br> </div>
 
-                        <div class="col-md-3">Url</div>
-                        <div class="col-md-9">
+                        <div class="col-md-2 font-weight-bold">Url</div>
+                        <div class="col-md-10">
                             <input class="form-control" type="url" placeholder="www.uct.cl" name="url" id="url">
                         </div>
 
                         <div class="col-md-12"> <br> </div>
 
-                        <div class="col-md-3">Fecha</div>
-                        <div class="col-md-9">
+                        <div class="col-md-2 font-weight-bold">Fecha</div>
+                        <div class="col-md-10">
                             <input class="col-md-12" type="date" name="fecha" required>
                         </div>
 
                         <div class="col-md-12"> <br> </div>
 
-                        <div class="col-md-3">¿Publico?</div>
-                        <div class="col-md-9">
+                        <div class="col-md-2 font-weight-bold">¿Publico?</div>
+                        <div class="col-md-10">
                             <input type="radio" name="publico" value="1" required> Si <br>
                             <input type="radio" name="publico" value="0"> No <br>
                         </div>
 
                         <div class="col-md-12"> <br> </div>
 
-                        <div class="col-md-9"></div>
-                        <div class="col-md-3 btn btn-primary">
+                        <div class="col-md-10"></div>
+                        <div class="col-md-2 btn btn-primary">
                             <button class="btn btn-primary" type="submit">Insertar</button>
                         </div>
                     </div>
