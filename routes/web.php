59e025007f75df2e0f32cf4a3da47decfe4d1072
patricionicolas <patricionicolas@live.cl>
2020-08-06 15:19:29 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@
 
 Auth::routes();
 Route::get('/', 'HomeController@index')->name('home');
-//Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
 
 //Post Controller
 Route::get('/createpost', 'PostController@create')->name('createpost');
