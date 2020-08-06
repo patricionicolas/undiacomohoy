030804c7cad7b56a4fdaedcd25065e42e7f65475
patricionicolas <patricionicolas@live.cl>
2020-08-06 14:55:02 -0400

    Extends botones nuevo y crear
    Se externaliza en archivo separado el boton para crear y ver todos los registros.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,44 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-@include('extras.informacion')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-11">
-            <a href="{{ url('/createpost') }}">
-                <button type="button" class="btn btn-primary btn-lg btn-block">
-                    Ingresar un nuevo registro
-                </button>
-            </a>
-        </div>
-        <div class="col-md-1">
-            <a href="{{ url('/createpost') }}">
-                <button type="button" class="btn btn-primary btn-lg btn-block">
-                    Ver
-                </button>
-            </a>
-        </div>
-    </div>
-</div>
+    @include('extras.informacion')
+    @include('include.new_and_list_register')
+    <br>
 
-<br>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">Este día en años anteriores</div>
 
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">Este día en años anteriores</div>
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
